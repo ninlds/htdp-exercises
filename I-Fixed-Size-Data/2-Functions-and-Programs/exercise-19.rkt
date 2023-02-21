8faf392c82889e47b7f959ff2b77186a0cb9b81e
= <nathannigolimas@gmail.com>
2023-02-20 22:59:37 -0300

    fix exercise 19

--- I-Fixed-Size-Data/2-Functions-and-Programs/exercise-19.rkt
+++ I-Fixed-Size-Data/2-Functions-and-Programs/exercise-19.rkt
@@ -3,8 +3,4 @@
 
 ;; function
 (define (string-insert str i)
-  (string-append (substring str 0  i ) "_" (substring str i)) )
-
-;; or
-(define (string-insert.v2 str i)
-  (string-append (substring str 0  (- i 1) ) "_" (substring str i)) )
+  (string-append (substring str 0 i) "_" (substring str i)) )
